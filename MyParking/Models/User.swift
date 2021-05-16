7b7e7d4ed1e1886a366572ce3154688e589a6ce4
inzi1812 <inzi1812@gmail.com>
2021-05-16 01:32:24 -0400

    User Struct Created

---
+++ MyParking/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MyParking
+//
+//  Created by Inzi Hussain on 2021-05-16.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct User {
+    @DocumentID var id: String?
+    var name : String
+    var licensePlateNum : String
+}
